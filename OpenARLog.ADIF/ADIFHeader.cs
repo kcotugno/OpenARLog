8148d4809160671ce5b31d68a60fe5700721e26d
kcotugno <kmcotugno@gmail.com>
2016-02-25 17:13:32 -0800

    Skeleton ADIWriter and ADIFHeader

---
+++ OpenARLog.ADIF/ADIFHeader.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Copyright (C) 2016 kcotugno
+ * Distributed under the MIT software license, see the accompanying
+ * LICENSE file or http://www.opensource.org/licenses/MIT
+ *
+ * Author: kcotugno
+ * Date: 2/24/2016
+ */
+
+using System;
+
+namespace OpenARLog.ADIF
+{
+    public class ADIFHeader
+    {
+        public string Version
+        {
+            get; set;
+        } 
+
+        public DateTime TimeStamp
+        {
+            get; set;
+        }
+
+        public string ProgramId
+        {
+            get; set;
+        }
+
+        public string ProgramVersion
+        {
+            get; set;
+        }
+    }
+}
---
+++ OpenARLog.ADIF/ADIWriter.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Copyright (C) 2016 kcotugno
+ * Distributed under the MIT software license, see the accompanying
+ * LICENSE file or http://www.opensource.org/licenses/MIT
+ *
+ * Author: kcotugno
+ * Date: 2/23/2016
+ */
+
+ // TODO
+
+namespace OpenARLog.ADIF
+{
+     class ADIWriter
+    {
+        public ADIWriter(string Program)
+        {
+
+        }
+    }
+}
