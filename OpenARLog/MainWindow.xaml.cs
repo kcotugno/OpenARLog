0acbfe3e653d332b559f37114d0354cd5d380cc3
kcotugno <kmcotugno@gmail.com>
2016-04-06 00:52:44 -0700

    Add UI support for Date and time input

--- OpenARLog/MainWindow.xaml.cs
+++ OpenARLog/MainWindow.xaml.cs
@@ -85,10 +85,17 @@ namespace OpenARLog
             Application.Current.Shutdown();
         }
         #endregion
-        
+
+        #region Main Ui Events
 
         private void logBtnClick(object sender, RoutedEventArgs e)
         {
+            if (IsValidTime(TimeOnTxt.Text) == false || IsValidDate(DateOnTxt.Text) == false)
+                return;
+
+            if (IsValidTime(TimeOffTxt.Text) == false || IsValidDate(DateOffTxt.Text) == false)
+                return;
+
             QSO contact = new QSO()
             {
                 Callsign = CallsignTxt.Text,
@@ -101,8 +108,8 @@ namespace OpenARLog
                 Band = BandTxt.Text,
                 Mode = ModeTxt.Text,
                 Frequency = FrequencyTxt.Text,
-                //TimeOn = TimeOffTxt.Text,
-                //TimeOff = TimeOffTxt.Text
+                DateTimeOn = GetDateTime(TimeOnTxt.Text, DateOnTxt.Text),
+                DateTimeOff = GetDateTime(TimeOffTxt.Text, DateOffTxt.Text)
             };
 
             _qsoLog.InsertQSO(contact);
@@ -121,6 +128,32 @@ namespace OpenARLog
                 LogBtn.IsEnabled = true;
         }
 
+        private void TimeOnGotFocus(object sender, RoutedEventArgs e)
+        {
+            if (TimeOnTxt.Text == string.Empty)
+                TimeOnTxt.Text = DateTime.Now.ToString("HH:mm:ss");
+        }
+
+        private void DateOnGotFocus(object sender, RoutedEventArgs e)
+        {
+            if (DateOnTxt.Text == string.Empty)
+                DateOnTxt.Text = DateTime.Now.ToString("MM/dd/yyyy");
+        }
+
+        private void TimeOffGotFocus(object sender, RoutedEventArgs e)
+        {
+            if (TimeOffTxt.Text == string.Empty)
+                TimeOffTxt.Text = DateTime.Now.ToString("HH:mm:ss");
+        }
+
+        private void DateOffGotFocus(object sender, RoutedEventArgs e)
+        {
+            if (DateOffTxt.Text == string.Empty)
+                DateOffTxt.Text = DateTime.Now.ToString("MM/dd/yyyy");
+        }
+
+        #endregion
+
         #region User Messages
 
         private void showTODOMessage()
@@ -130,5 +163,83 @@ namespace OpenARLog
 
         #endregion
 
+        #region Helper Functions
+
+        private bool IsValidDateOrTime(string check, char delimiter)
+        {
+            if (check == string.Empty)
+                return true;
+
+            if (check.Length == 2 || check.Length == 3)
+            {
+                for (int i = 0; i < check.Length; i++)
+                {
+                    try
+                    {
+                        Convert.ToInt32(check[i]);
+                    }
+                    catch
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        private bool IsValidTime(string time)
+        {
+            return IsValidDateOrTime(time, ':');
+        }
+
+        private bool IsValidDate(string date)
+        {
+            return IsValidDateOrTime(date, '/');
+        }
+
+        private DateTime? GetDateTime(string time, string date)
+        {
+            if (time == string.Empty && date == string.Empty)
+                return null;
+
+            int year = 1500;
+            int month = 1;
+            int day = 1;
+            int hour = 0;
+            int minutes = 0;
+            int seconds = 0;
+
+            string[] _date = date.Split('/');
+            string[] _time = time.Split(':');
+
+            DateTime datetime;
+            
+            try
+            {
+                month = Convert.ToInt32(_date[0]);
+                day = Convert.ToInt32(_date[1]);
+                year = _date.Length == 3 ? Convert.ToInt32(_date[2]) : 2016;
+
+                // If the year is in 2 digit form, we will just assume it is a 2000 year.
+                if (year < 100)
+                    year += 2000;
+
+                hour = Convert.ToInt32(_time[0]);
+                minutes = Convert.ToInt32(_time[1]);
+                seconds = _time.Length == 3 ? Convert.ToInt32(_time[2]) : 0;
+
+                datetime = new DateTime(year, month, day, hour, minutes, seconds);
+            }
+            catch
+            {
+                return null;
+            }
+
+
+            return datetime;
+        }
+
+        #endregion
     }
 }
