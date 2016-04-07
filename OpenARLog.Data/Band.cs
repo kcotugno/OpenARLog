db44a594079b3bd44b8aeae533333fb32fc1dab6
kcotugno <kmcotugno@gmail.com>
2016-04-07 13:21:02 -0700

    Rewrite Bands

--- OpenARLog.Data/Band.cs
+++ OpenARLog.Data/Band.cs
@@ -14,40 +14,72 @@ namespace OpenARLog.Data
     sealed class BAND
     {
         // Varibles cannot have a number as the first character. So we reverse the meter position.
-        public static string M2190 = "2190m";
-        public static string M630 = "630m";
-        public static string M560 = "560m";
-        public static string M160 = "160m";
-        public static string M80 = "80m";
-        public static string M60 = "60m";
-        public static string M40 = "40m";
-        public static string M30 = "30m";
-        public static string M20 = "20m";
-        public static string M17 = "17m";
-        public static string M15 = "15m";
-        public static string M12 = "12m";
-        public static string M10 = "10m";
-        public static string M6 = "6m";
-        public static string M4 = "4m";
-        public static string M2 = "2m";
-        public static string M1_25 = "1.25m";
-        public static string CM70 = "70cm";
-        public static string CM33 = "33cm";
-        public static string CM23 = "23cm";
-        public static string CM13 = "13cm";
-        public static string CM9 = "9cm";
-        public static string CM6 = "6cm";
-        public static string CM3 = "3cm";
-        public static string CM1_25 = "1.25cm";
-        public static string MM6 = "6mm";
-        public static string MM4 = "4mm";
-        public static string MM2_5 = "2.5mm";
-        public static string MM2 = "2mm";
-        public static string MM1 = "1mm";
+        public enum BANDS
+        {
+            M2190 = 0,
+            M630,
+            M560,
+            M160,
+            M80,
+            M60,
+            M40,
+            M30,
+            M20,
+            M17,
+            M15,
+            M12,
+            M10,
+            M6,
+            M4,
+            M2,
+            M1_25,
+            CM70,
+            CM33,
+            CM23,
+            CM13,
+            CM9,
+            CM6,
+            CM3,
+            CM1_25,
+            MM6,
+            MM4,
+            MM2_5,
+            MM2,
+            MM1
+    };
 
-        private static string[] all = { M2190, M630, M560, M160, M80, M60, M40, M30, M20, M17, M15,
-                                        M12, M10, M6, M4, M2, M1_25, CM70, CM33, CM23, CM13, CM9,
-                                        CM6, CM3, CM1_25, MM6, MM4, MM2_5, MM2, MM1 };
+
+        private static string[] all = { "2190m",
+                                        "630m",
+                                        "560m",
+                                        "160m",
+                                        "80m",
+                                        "60m",
+                                        "40m",
+                                        "30m",
+                                        "20m",
+                                        "17m",
+                                        "15m",
+                                        "12m",
+                                        "10m",
+                                        "6m",
+                                        "4m",
+                                        "2m",
+                                        "1.25m",
+                                        "70cm",
+                                        "33cm",
+                                        "23cm",
+                                        "13cm",
+                                        "9cm",
+                                        "6cm",
+                                        "3cm",
+                                        "1.25cm",
+                                        "6mm",
+                                        "4mm",
+                                        "2.5mm",
+                                        "2mm",
+                                        "1mm"
+                                        };
 
         public static bool IsValidBand(string text)
         {
