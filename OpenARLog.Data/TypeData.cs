79100f7c42d595c42e54d3ec234bb43012169982
kcotugno <kmcotugno@gmail.com>
2016-04-09 00:16:29 -0700

    Add TypeData abstract class

---
+++ OpenARLog.Data/TypeData.cs
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2015-2016 kcotugno
+ * All rights reserved
+ *
+ * Distributed under the terms of the BSD 2 Clause software license. See the
+ * accompanying LICENSE file or http://www.opensource.org/licenses/BSD-2-Clause.
+ *
+ * Author: kcotugno
+ * Date: 4/6/2016
+ */
+
+using System.Data;
+
+namespace OpenARLog.Data
+{
+    public abstract class TypeData
+    {
+        protected dataTable;
+        protected typeDataDb;
+        protected Constants.TYPES type;
+
+        public Load()
+        {
+            dataTable = typeDataDb.GetTable(type);
+        }
+
+        public DataTable GetTypeData()
+        {
+            return dataTable;
+        }
+    }
+}
