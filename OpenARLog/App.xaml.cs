de6d30a6e28c86232e8684cf2606bbe8bafd7b35
kcotugno <kmcotugno@gmail.com>
2016-04-13 22:06:40 -0700

    Remove unnecessary references

--- OpenARLog/App.xaml.cs
+++ OpenARLog/App.xaml.cs
@@ -9,12 +9,6 @@
  * Date: 8/15/2015
  */
 
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace OpenARLog
